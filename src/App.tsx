d318f9ec577e8cf43acdce92b38fc65b5d5792c0
Knight-Codes <bishal@orbittechlabs.com>
2025-03-02 13:43:28 +0530

    feat: adds shadcn components to platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export const App = () => {
   return (
     <div className="w-screen h-screen bg-black flex items-center justify-center">
-      <p className="text-3xl font-manrope text-slate-100 tracking-wider">HENBYTEQ</p>
+      <p className="text-3xl font-manrope text-slate-100 tracking-wider">
+        HENBYTEQ
+      </p>
     </div>
   );
 };
