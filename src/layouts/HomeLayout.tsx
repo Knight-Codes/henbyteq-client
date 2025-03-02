7a24f29f134cd9988eed68d561f2e36dc6567b17
Knight-Codes <bishal@orbittechlabs.com>
2025-03-02 13:51:02 +0530

    feat: Separate app into route-based structure

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export const HomeLayout = () => {
   return (
     <div className="w-screen h-screen bg-black flex items-center justify-center">
       <p className="text-3xl font-manrope text-slate-100 tracking-wider">
